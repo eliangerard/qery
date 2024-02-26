872b8b15840de852523c76fe7a2eb1bfcf9507ae
eliangerard <eliangerardgg@gmail.com>
2024-02-25 20:32:50 -0600

    Added font and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily : {
+      'sans' : ['Prompt', 'sans-serif']
+    },
+    extend: {
+      colors : {
+        'azul' : {
+          100 : '#80E9FF',
+          200 : '#00D4FF',
+          300 : '#0A2540'
+        },
+        'magenta' : {
+          100 : '#FF80FF',
+          200 : '#FF00FF',
+          300 : '#3D0A42'
+        },
+        'w' : {
+          100 : '#FEF0FC'
+        }
+      }
+    },
   },
   plugins: [],
 }
