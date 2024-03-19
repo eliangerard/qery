1715f1083efca72d24e109e24157a8d4e7e3c636
alexarrechz <ivanalejandroarreolachavez@gmail.com>
2024-03-19 14:54:32 -0600

    Added users admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Landing } from './pages/Landing';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login';
+import Users from './pages/admin/Users';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/' element={<Landing />} />
+          <Route path='/admin/users' element={<Users />} />
         </Routes>
       </BrowserRouter>
     </>
