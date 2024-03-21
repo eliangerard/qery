331ec7d10cf297d9c4a0ec9a5edfcc2e32304515
eliangerard <eliangerardgg@gmail.com>
2024-03-20 22:03:19 -0600

    Aded new icons

---
+++ src/ui/Icons/Messages.jsx
@@ -0,0 +1,13 @@
+export const Messages = ({className}) => {
+    return (
+        <svg className={className} viewBox="0 0 1055 1334" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect y="790" width="781.739" height="119.526" transform="rotate(-6.64961 0 790)" fill="#F5B000" />
+            <rect y="1215" width="781.739" height="119.526" transform="rotate(-6.64961 0 1215)" fill="#F5B000" />
+            <rect y="332.523" width="781.739" height="119.526" transform="rotate(-6.64961 0 332.523)" fill="#F5B000" />
+            <rect x="278" y="911" width="781.739" height="119.526" transform="rotate(6.84905 278 911)" fill="#0061B6" />
+            <rect x="278" y="486" width="781.739" height="119.526" transform="rotate(6.84905 278 486)" fill="#0061B6" />
+            <rect x="278" y="0.61377" width="781.739" height="119.526" transform="rotate(6.84905 278 0.61377)" fill="#0061B6" />
+        </svg>
+
+    )
+}
