f0ee10ec1b79216689fa11ed2d12bec4df0d78a8
eliangerard <eliangerardgg@gmail.com>
2024-03-20 22:01:31 -0600

    Created user context and its provider

---
+++ src/context/UserProvider.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import UserContext from './UserContext';
+
+
+export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
