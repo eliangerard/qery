fa3bae4b9aa11c0f64c7708feb96ad5f11862dc7
eliangerard <eliangerardgg@gmail.com>
2024-03-20 22:02:17 -0600

    Remaked landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,69 +1,59 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { Aside } from "../ui/Icons/Aside"
+import { Messages } from "../ui/Icons/Messages"
+import { GuyL } from "../ui/Icons/GuyL"
+import { GuyR } from "../ui/Icons/GuyR"
+import { useGoogleLogin } from "@react-oauth/google"
 
 export const Landing = () => {
+
+    const navigate = useNavigate();
+
+    const login = useGoogleLogin({
+        onSuccess: async tokenResponse => {
+            localStorage.setItem('token', tokenResponse.access_token);
+            navigate('/home');
+        }
+    });
+
     return (
-        <div className="bg-w-100 w-full min-h-screen h-fit">
+        <div className="flex relative items-center h-full overflow-hidden">
+            <Aside className="min-h-screen h-screen px-4" />
+            <div className="relative z-10">
+                <h1 className="text-8xl 2xl:text-[10rem] font-bold left-[-5.8rem] 2xl:left-[-9rem] relative">Qery.me</h1>
+                <div className="w-96 2xl:w-[32rem]">
+                    <p className="text-2xl 2xl:text-4xl my-3 2xl:my-8 ">Conéctate con tus clientes al instante</p>
+                    <button className="flex items-center justify-center bg-ab-500 text-white w-fit text-center font-bold p-4 px-12 text-xl 2xl:text-2xl"
+                        onClick={login}
+                    >
+                        <svg className="h-7 mr-4" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
 
-            <header className="fixed top-0 left-0">
-                <div className="relative w-fit flex">
-                    <div className="w-fit h-fit relative">
-                        <h1 className="absolute left-4 md:left-auto md:right-36 translate-y-1/2 bottom-1/2 text-white text-3xl 2xl:text-4xl font-extrabold w-fit">Qery.me</h1>
-                        <svg className="h-16 md:h-28 2xl:h-40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1308 335" fill="none">
-                            <path d="M972.425 0L1308 335L1140.54 335L0 335L0 0L972.425 0Z" fill="#0A2540" />
+                            <title>google [#178]</title>
+                            <desc>Created with Sketch.</desc>
+                            <defs>
+
+                            </defs>
+                            <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                <g id="Dribbble-Light-Preview" transform="translate(-300.000000, -7399.000000)" fill="#FFFFFF">
+                                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                                        <path d="M263.821537,7247.00386 L254.211298,7247.00386 C254.211298,7248.0033 254.211298,7250.00218 254.205172,7251.00161 L259.774046,7251.00161 C259.560644,7252.00105 258.804036,7253.40026 257.734984,7254.10487 C257.733963,7254.10387 257.732942,7254.11086 257.7309,7254.10986 C256.309581,7255.04834 254.43389,7255.26122 253.041161,7254.98137 C250.85813,7254.54762 249.130492,7252.96451 248.429023,7250.95364 C248.433107,7250.95064 248.43617,7250.92266 248.439233,7250.92066 C248.000176,7249.67336 248.000176,7248.0033 248.439233,7247.00386 L248.438212,7247.00386 C249.003881,7245.1669 250.783592,7243.49084 252.969687,7243.0321 C254.727956,7242.65931 256.71188,7243.06308 258.170978,7244.42831 C258.36498,7244.23842 260.856372,7241.80579 261.043226,7241.6079 C256.0584,7237.09344 248.076756,7238.68155 245.090149,7244.51127 L245.089128,7244.51127 C245.089128,7244.51127 245.090149,7244.51127 245.084023,7244.52226 L245.084023,7244.52226 C243.606545,7247.38565 243.667809,7250.75975 245.094233,7253.48622 C245.090149,7253.48921 245.087086,7253.49121 245.084023,7253.49421 C246.376687,7256.0028 248.729215,7257.92672 251.563684,7258.6593 C254.574796,7259.44886 258.406843,7258.90916 260.973794,7256.58747 C260.974815,7256.58847 260.975836,7256.58947 260.976857,7256.59047 C263.15172,7254.63157 264.505648,7251.29445 263.821537,7247.00386" id="google-[#178]">
+
+                                        </path>
+                                    </g>
+                                </g>
+                            </g>
                         </svg>
-                    </div>
-                    <div className="flex flex-col">
-                        <div className="bg-azul-300 flex-grow skew-x-[45deg] origin-bottom">
-                        </div>
-                        <Link to={"/login"} className="flex items-center justify-center bg-azul-100 text-xl font-bold w-40 2xl:w-48 h-10 md:h-14 2xl:h-20 ">Login</Link>
-                    </div>
+                        <p className="mr-4">Inicia con Google</p>
+                    </button>
                 </div>
-            </header>
-
-            <main className="flex flex-col md:flex-row items-center justify-center items-center min-h-screen h-full w-full md:px-[15%]">
-                <div className="w-full md:w-7/12 p-6 md:p-0 pt-28 md:pt-36">
-                    <h2 className="text-5xl md:text-6xl 2xl:text-8xl font-bold text-azul-300">Conéctate con tus clientes al instante</h2>
-                    <p className="text-2xl 2xl:text-4xl py-6 text-azul-300">Crea tu cuenta ahora mismo</p>
-                    <button className="bg-magenta-200 text-lg 2xl:text-xl text-w-100 font-bold w-36 2xl:w-56 h-14 2xl:h-20">Regístrate</button>
+            </div >
+            <div className="absolute left-2/4 flex h-full w-2/5 flex-col justify-end">
+                <Messages className="flex-1 w-full pt-8" />
+                <div className="flex w-full justify-between items-end">
+                    <GuyL className="w-80" />
+                    <GuyR className="w-80" />
                 </div>
-                <div className="flex w-full md:w-5/12 min-w-fit md:min-h-screen h-full justify-center md:justify-start">
-                    <div className="flex flex-col w-fit justify-around items-center">
-                        {[...Array(4)].map((_, i) => (
-                            <div key={`q${i}`} className={`-scale-x-100 w-fit text-4xl 2xl:text-6xl text-azul-300 ${i != 0 ? 'py-8' : ''}`}>
-                                <b>Q</b>
-                            </div>
-                        ))}
-                        <div className="relative w-fit flex items-center justify-center">
-                            <svg className="w-48 2xl:w-56" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471 440" fill="none">
-                                <path d="M470.552 219.729C470.552 341.082 367.889 439.458 241.248 439.458H0V355.866H71.6577C26.2745 312.875 11.9429 271.144 11.9429 219.729C11.9429 98.3761 114.606 0 241.248 0C367.889 0 470.552 98.3761 470.552 219.729Z" fill="#0A2540" />
-                            </svg>
-                            <p className="absolute text-w-100 text-3xl 2xl:text-4xl font-medium">¡Claro!</p>
-                        </div>
-                        <div className="-scale-x-100 w-fit text-4xl 2xl:text-6xl text-azul-300">
-                            <b>Q</b>
-                        </div>
-                    </div>
-                    <div className="flex flex-col w-fit justify-around items-center">
-                        {[...Array(3)].map((_, i) => (
-                            <div key={`q${i}`} className={`w-fit text-4xl 2xl:text-6xl text-azul-200  ${i != 0 ? 'py-8' : ''}`}>
-                                <b>Q</b>
-                            </div>
-                        ))}
-                        <div className="relative w-fit flex items-center justify-center">
-                            <svg className="w-48 2xl:w-56" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471 440" fill="none">
-                                <path d="M0.385284 219.729C0.385284 341.082 103.048 439.458 229.69 439.458H470.937V355.866H399.28C444.663 312.875 458.994 271.144 458.994 219.729C458.994 98.3761 356.331 0 229.69 0C103.048 0 0.385284 98.3761 0.385284 219.729Z" fill="#00D4FF" />
-                            </svg>
-                            <p className="absolute text-w-100 text-3xl 2xl:text-4xl text-center text-azul-300 font-medium">¿Abrirán hoy?</p>
-                        </div>
-                        {[...Array(2)].map((_, i) => (
-                            <div key={`q${i}`} className={`w-fit text-4xl 2xl:text-6xl text-azul-200  ${i == 0 ? 'py-8' : ''}`}>
-                                <b>Q</b>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </main>
-
-        </div>
+            </div>
+        </div >
     )
 }
