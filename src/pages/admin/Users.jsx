002f4e7b33633108eaa9d64ace65a7a0bd9d05d5
eliangerard <eliangerardgg@gmail.com>
2024-03-20 22:02:40 -0600

    Updated users fetching and UI

--- src/pages/admin/Users.jsx
+++ src/pages/admin/Users.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import User from "../../ui/admin/User";
+import { Logo } from "../../ui/Icons/Logo";
+import { useNavigate } from "react-router-dom";
 
 const Users = () => {
+  const navigate = useNavigate();
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.qery.me/users")
+    fetch("https://api.qery.me/users/all")
       .then((res) => res.json())
       .then((res) => {
         setUsers(res);
@@ -15,14 +18,19 @@ const Users = () => {
 
   return (
     <div>
-      <header className="w-full py-8 px-12 bg-b-500">
-        <h1 className="font-bold text-4xl text-white">Empresas registradas</h1>
-      </header>
-      <div className="w-full flex justify-center min-h-full">
-        <div className="flex flex-wrap w-4/5">
-          {users.map((user, i) => (
-            <User key={i} user={user} />
-          ))}
+      <header className="w-full flex items-center h-24 bg-b-500">
+        <button className="h-full" onClick={() => navigate("/")}>
+          <Logo className="h-full" />
+        </button>
+        <h1 className="font-bold text-4xl ml-8 text-white">Empresas registradas</h1>
+      </header><div className="w-full p-4">
+        <h2 className="text-center">Mostrando {users.length} usuarios</h2>
+        <div className="w-full flex justify-center min-h-full">
+          <div className="flex flex-wrap w-full md:w-4/5">
+            {users.map((user, i) => (
+              <User key={i} user={user} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
