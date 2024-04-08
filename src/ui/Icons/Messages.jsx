66746e7f9bee24022e3b548305808f2086b3bb18
eliangerard <eliangerardgg@gmail.com>
2024-04-08 16:33:28 -0600

    Add responsive alternatives

--- src/ui/Icons/Messages.jsx
+++ src/ui/Icons/Messages.jsx
@@ -1,4 +1,4 @@
-export const Messages = ({className}) => {
+export const Messages = ({ className }) => {
     return (
         <svg className={className} viewBox="0 0 1055 1334" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect y="790" width="781.739" height="119.526" transform="rotate(-6.64961 0 790)" fill="#F5B000" />
@@ -11,3 +11,16 @@ export const Messages = ({className}) => {
 
     )
 }
+
+export const ResponsiveMessages = ({ className }) => {
+    return (
+        <svg className={className} viewBox="0 0 438 680" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="116" y="445.23" width="229.226" height="35.0481" transform="rotate(-6.64961 116 445.23)" fill="#F5B000" />
+            <rect x="197.517" y="347.906" width="229.226" height="35.0481" transform="rotate(6.84905 197.517 347.906)" fill="#0061B6" />
+            <rect y="627.367" width="229.226" height="35.0481" transform="rotate(-6.64961 0 627.367)" fill="#F5B000" />
+            <rect x="81.5166" y="530.043" width="229.226" height="35.0481" transform="rotate(6.84905 81.5166 530.043)" fill="#0061B6" />
+            <rect x="281" y="263.094" width="229.226" height="35.0481" transform="rotate(-6.64961 281 263.094)" fill="#F5B000" />
+            <rect x="362.517" y="173.953" width="229.226" height="35.0481" transform="rotate(6.84905 362.517 173.953)" fill="#0061B6" />
+        </svg>
+    )
+};
