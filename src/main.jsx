24083e5951dc2e83c6941fc6ab6bd307df815d8f
eliangerard <eliangerardgg@gmail.com>
2024-04-09 00:02:54 -0600

    Disabled react strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -6,8 +5,6 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId='131255106642-pnrhidgpc4irnd8b4v8gcpkot235v60i.apps.googleusercontent.com'>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </GoogleOAuthProvider>
 )
