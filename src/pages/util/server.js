0a05559ceb3ee1fc046bde164f851fe5d3fe2b9d
eliangerard <eliangerardgg@gmail.com>
2024-04-09 00:06:57 -0600

    Changed server logic

--- src/pages/util/server.js
+++ src/pages/util/server.js
@@ -1,2 +1,2 @@
-export const server = "https://api.qery.me/";
-//export const server = "http://localhost:3000/";
+export const server = "https://api.qery.me";
+//export const server = "http://localhost:3000";
