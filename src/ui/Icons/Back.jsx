64f1e2f3a9ad3407b899505ea3269058cee9b939
eliangerard <eliangerardgg@gmail.com>
2024-04-09 00:07:37 -0600

    Created Back icon

---
+++ src/ui/Icons/Back.jsx
@@ -0,0 +1,8 @@
+export const Back = ({ className }) => {
+    return (
+        <svg className={className} viewBox="0 0 48 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3.36603 31.5927C0.0687308 29.6614 0.0687249 24.8952 3.36602 22.9639L40.223 1.37615C43.5562 -0.576189 47.75 1.82764 47.75 5.69055L47.75 48.8661C47.75 52.729 43.5562 55.1328 40.223 53.1805L3.36603 31.5927Z" fill="black" />
+        </svg>
+
+    )
+}
