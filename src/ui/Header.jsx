4b348a202a8adef90de1f625941895ce0d41e599
eliangerard <eliangerardgg@gmail.com>
2024-04-09 22:54:06 -0600

    Created header component

---
+++ src/ui/Header.jsx
@@ -0,0 +1,49 @@
+import { useContext, useState } from "react";
+import { Share } from "./Icons/Share";
+import { Link } from "react-router-dom";
+import { Logo } from "./Icons/Logo";
+import UserContext from "../context/UserContext";
+
+export const Header = ({showLeft, setShowLeft, showRight, setShowRight}) => {
+
+    const { user } = useContext(UserContext);
+
+    const [showCopied, setShowCopied] = useState(false);
+
+    return (
+        <div className="flex absolute top-0 left-0 w-full justify-between">
+            <div className="flex relative">
+                <button className={`flex relative z-30 lg:hidden items-center justify-center pt-0.5 w-12 h-12 text-xl bg-accent-500 font-bold text-accent-500 transition-all`}
+                    onClick={() => setShowLeft(show => !show)}
+                >
+                    <div className="rounded-full flex items-center justify-center bg-black text-sm h-6 w-6">i</div>
+                </button>
+                <div className="relative flex h-12">
+                    <button className="relative bg-ab-500 w-12 h-12 flex items-center justify-center pr-1 pt-1 rounded-r-full hover:translate-x-[0.5rem] active:translate-x-[0.2rem] transition-all duration-75 z-10"
+                        onClick={() => {
+                            const company = encodeURI(user?._id)
+                            const link = "https://qery.me/company/" + company;
+                            navigator.clipboard.writeText(link);
+                            setShowCopied(true);
+                            setTimeout(() => setShowCopied(false), 2000);
+                        }}
+                    >
+                        <div className={`${showCopied ? "opacity-100 max-h-8 p-2" : "opacity-0 max-h-0"} absolute bg-white w-40 overflow-hidden right-[-100%] translate-x-[75%] top-1/2 translate-y-[-50%] rounded-full transition-all`}>
+                            <p className="font-semibold text-sm">¡Enlace copiado!</p>
+                        </div>
+                        <Share className="w-4" />
+                    </button>
+                    <div className="absolute w-4 bg-ab-500 h-12 left-0 top-0"></div>
+                </div>
+            </div>
+            <div className="flex">
+                <Link to="/">
+                    <Logo className="h-12" />
+                </Link>
+                <button className={`${showRight ? "right-[28rem]" : "right-0"} xl:hidden z-10 h-12 w-12 flex items-center justify-center bg-b-500 text-white font-bold transition-all`}
+                    onClick={() => setShowRight(show => !show)}
+                >FAQ</button>
+            </div>
+        </div>
+    )
+}
