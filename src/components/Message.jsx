418a66cbd595f0501de44051c45ff982dc037c73
eliangerard <eliangerardgg@gmail.com>
2024-05-26 12:13:17 -0600

    Add AI answered message style

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,8 @@
-export const Message = ({ content, mine }) => {
+export const Message = ({ content, mine, user }) => {
     return (
         <div className={`flex ${mine ? "justify-end" : "justify-start"} w-full`}>
             <div className="flex max-w-[90%] justify-between mb-2">
-                <p className={`${mine ? "bg-accent-500" : "bg-b-500 text-white"} flex-1 break-all text-pretty max-w-full items-center px-2 py-1`}>{content}</p>
+                <p className={`${user.ai ? "bg-green-500 text-white" : (mine ? "bg-accent-500" : "bg-b-500 text-white")} flex-1 break-words text-pretty max-w-full items-center px-2 py-1`}>{content}</p>
             </div>
         </div>
     )
