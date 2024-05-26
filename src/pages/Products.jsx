98ba8d2406f838833621b2199b12d1d0bbba2b72
eliangerard <eliangerardgg@gmail.com>
2024-05-26 17:29:06 -0600

    Update API call and product component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,16 +2,21 @@ import { useContext, useEffect, useState } from "react";
 import { server } from "./util/server";
 import UserContext from "../context/UserContext";
 import { SquareLoader } from "react-spinners";
+import { useLocation } from "react-router-dom";
+import { Product } from "../ui/Product";
 
 export const Products = () => {
 
     const { user } = useContext(UserContext);
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
-
+    
+    const { pathname } = useLocation();
+    console.log(window.location);
+    
     useEffect(() => {
         setLoading(true);
-        fetch(`${server}/users/account/products`, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
+        fetch(`${server}/users/account/products/${user ? user._id : pathname.substring(1, pathname.substring(1).indexOf('/'))}`, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
             .then(res => {
                 return res.json();
             })
@@ -69,40 +74,13 @@ export const Products = () => {
                         Vincula tu cuenta</button>
                 </div>}
             {loading ?
-                <div className="flex items-center justify-center my-8 w-full">
+                <div className="flex items-center justify-center  h-96 w-full">
                     <SquareLoader color="#000" size={50} />
                 </div>
                 :
-                <div className="grid grid-cols-3 lg:grid-cols-2 xl:grid-cols-1 2xl:grid-cols-2 gap-6">
+                <div className="grid sm:grid-cols-2 xl:grid-cols-1 2xl:grid-cols-2 gap-6">
                     {products.map(product => (
-                        <div key={product.id} className="flex items-center justify-between w-full">
-                            <div>
-                                <img className="min-h-32 h-full w-32 object-cover" src={product.images[0]} alt="" />
-                            </div>
-                            <div className="flex flex-col justify-between h-full flex-1">
-                                <div className="p-2">
-                                    <p className="font-bold text-xl leading-tight">{product.name}</p>
-                                    <p className="font-medium">1 unidad</p>
-                                </div>
-                                <p>{product.price}</p>
-                                <button
-                                    className="bg-green-500 text-white font-bold w-full text-lg h-10"
-                                    onClick={() => {
-                                        fetch(`${server}/users/checkout/`, {
-                                            method: "POST",
-                                            headers: {
-                                                'Content-Type': 'application/json',
-                                                Authorization: `Bearer ${localStorage.getItem('token')}`
-                                            },
-                                            body: JSON.stringify({ priceId: product.default_price })
-                                        }).then(res => res.json()).then(res => {
-                                            if (res.error) return console.error(res.error);
-                                            console.log(res);
-                                            window.location.href = res.session.url;
-                                        })
-                                    }}>${product.prices.unit_amount.toString().substring(0, product.prices.unit_amount.toString().length - 2).toLocaleString('es-MX')}</button>
-                            </div>
-                        </div>
+                        <Product key={product._id} {...product} />
                     ))}
                 </div>
             }
