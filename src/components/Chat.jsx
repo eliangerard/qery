0c59de743f2fab28a41755acae66a2505dc2aac1
eliangerard <eliangerardgg@gmail.com>
2024-05-26 12:13:03 -0600

    Add viewed message style

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom"
 
 export const Chat = ({ _id: id, user, lastMessage, newMessages }) => {
     return (
-        <Link to={`/chat/${id}`} className="w-full">
-            <div className="flex w-full justify-between mb-2 hover:cursor-pointer">
-                <p className="w-[150px] text-center bg-ab-500 py-1 text-white font-semibold">{user}</p>
-                <p className="bg-accent-500 truncate flex-1 items-center px-2 py-1">{lastMessage.content}</p>
-                <p className="bg-ab-500 w-8 max-8 text-white font-semibold flex items-center justify-center">{newMessages}</p>
+        <Link to={`/chat/${id}`} className={`w-full mb-2 `}>
+            <div className={`flex w-full justify-between hover:cursor-pointer ${!lastMessage.user.name ? 'opacity-75' : '' }`}>
+                <p className={`w-[200px] text-center bg-ab-500 py-1 text-white ${!lastMessage.user.name ? 'font-regular' : 'font-semibold' }`}>{user}</p>
+                <p className={`bg-accent-500 truncate flex-1 items-center px-2 py-1 ${!lastMessage.user.name ? 'opacity-75' : '' }`}>{lastMessage.content}</p>
+                {lastMessage.user.name && <p className="bg-ab-500 w-8 max-8 text-white font-semibold flex items-center justify-center">{newMessages}</p>}
             </div>
         </Link>
     )
