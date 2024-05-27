60148a5791e95ed322aefe28c64ecc6228c40b6d
eliangerard <eliangerardgg@gmail.com>
2024-05-27 17:53:43 -0600

    Add product sended exception and fix box sizing

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom"
 
 export const Chat = ({ _id: id, user, lastMessage, newMessages }) => {
     return (
-        <Link to={`/chat/${id}`} className={`w-full mb-2 `}>
-            <div className={`flex w-full justify-between hover:cursor-pointer ${!lastMessage.user.name ? 'opacity-75' : '' }`}>
-                <p className={`w-[200px] text-center bg-ab-500 py-1 text-white ${!lastMessage.user.name ? 'font-regular' : 'font-semibold' }`}>{user}</p>
-                <p className={`bg-accent-500 truncate flex-1 items-center px-2 py-1 ${!lastMessage.user.name ? 'opacity-75' : '' }`}>{lastMessage.content}</p>
+        <div className={`w-full mb-2 `}>
+            <Link to={`/chat/${id}`} className={`flex w-full justify-between hover:cursor-pointer ${!lastMessage.user.name ? 'opacity-75' : ''}`}>
+                <p className={`w-[200px] text-center bg-ab-500 py-1 text-white ${!lastMessage.user.name ? 'font-regular' : 'font-semibold'}`}>{user}</p>
+                <p className={`bg-accent-500 truncate flex-1 items-center px-2 py-1 ${!lastMessage.user.name ? 'opacity-75' : ''}`}>
+                    {lastMessage.content.startsWith("@prod") ? <i>Producto enviado</i> : lastMessage.content}
+                </p>
                 {lastMessage.user.name && <p className="bg-ab-500 w-8 max-8 text-white font-semibold flex items-center justify-center">{newMessages}</p>}
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
