2bf779280a355090866145a2471c2a52168b363e
eliangerard <eliangerardgg@gmail.com>
2024-05-30 01:34:15 -0600

    Update Store component to conditionally render the store navigation based on user's stripe account status

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,18 +1,21 @@
 import { Link, Route, Routes } from "react-router-dom"
 import { Products } from "./Products"
 import { Orders } from "./Orders"
+import UserContext from "../context/UserContext";
+import { useContext } from "react";
 
 export const Store = () => {
+    const {user} = useContext(UserContext);
     return (
         <>
-            <div className="flex w-full text-4xl font-bold my-4 mt-8">
+            {user.stripeAccount && <div className="flex w-full text-4xl font-bold my-4 mt-8">
                 <Link to="" className="mr-12">
                     <h2>Productos</h2>
                 </Link>
                 <Link to="orders">
                     <h2>Ordenes</h2>
                 </Link>
-            </div>
+            </div>}
             <Routes>
                 <Route path="orders" element={<Orders popup />} />
                 <Route path="*" element={<Products popup />} />
